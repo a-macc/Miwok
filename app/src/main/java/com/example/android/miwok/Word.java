0d2dbeb92c9acfd41330116b698b1372eef2a0bf
Anurag Makade <iit2018121@iiita.ac.in>
2019-03-20 22:57:47 +0530

    Add audio pronunciations

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -4,16 +4,20 @@ public class Word {
     private String mDefaultTranslation;
     private String mMiwokTranslation;
     private int mImageResourceId = -1;
+    private int mAudioResourceId;
 
-    public Word(String defaultTranslation, String miwokTranslation){
+    public Word(String defaultTranslation, String miwokTranslation, int audioId){
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
+        mAudioResourceId = audioId;
     }
 
-    public Word(String defaultTranslation, String miwokTranslation, int ImageResourceId){
+    public Word(String defaultTranslation, String miwokTranslation, int ImageResourceId,
+                int audioId){
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
         mImageResourceId = ImageResourceId;
+        mAudioResourceId = audioId;
     }
 
     public String getDefaultTranslation(){
@@ -26,6 +30,10 @@ public class Word {
 
     public int getImageResourceId(){
         return mImageResourceId;
+    }
+
+    public int getAudioResourceId(){
+        return mAudioResourceId;
     }
 
     public boolean hasImage(){
